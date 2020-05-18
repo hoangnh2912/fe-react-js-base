88e18b6f9d6f0eb70b1845430b0ea38e9f6b4911
Hoàng <hoangnh5901@outlook.com>
2020-05-18 19:14:01 +0700

    Updates

---
+++ precache-manifest.80bf113d3d50faf7bc4e13e0b2a45357.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12eac311eb6afae275614024d74a5564",
+    "url": "./index.html"
+  },
+  {
+    "revision": "443d4dda82ab1ceb5608",
+    "url": "./static/js/2.b7389038.chunk.js"
+  },
+  {
+    "revision": "32ac666b7c7d71f2807a8e3c52e8a26a",
+    "url": "./static/js/2.b7389038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72034fb20356aec1d96",
+    "url": "./static/js/main.6aa01038.chunk.js"
+  },
+  {
+    "revision": "9887955c5f831c350791",
+    "url": "./static/js/runtime-main.7f87e6f1.js"
+  }
+]);
